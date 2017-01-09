911e9a957881f30e7c9cb5d4d73fa0501dce38c4
zw <895150121@qq.com>
2017-01-09 09:39:10 +0800

    modified sh

--- sh/server_amcl.sh
+++ sh/server_amcl.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-
-sudo chmod 777 /dev/ttyUSB0
-sudo chmod 777 /dev/ttyUSB1
-
 MASTER_IP=192.168.1.100:11311
 
 source devel/setup.bash
 export ROS_MASTER_URI=http://$MASTER_IP
-roslaunch a_robot_platform config.launch 
+roslaunch amcl orange_diff.launch 
